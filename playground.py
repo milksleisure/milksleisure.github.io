c2509474ae984d27c2d8cad0ed54213269940427
Bing-Ru.Wu <killuaken@gmail.com>
2018-01-01 09:58:21 +0100

    Add static test by SimpleHTTPServer

--- playground.py
+++ playground.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 from flask_frozen import Freezer
 from playground.utils import options
 import sys
+import os
+import SimpleHTTPServer
+import SocketServer
 
 app = Flask(__name__)
 
@@ -10,7 +13,7 @@ app = Flask(__name__)
 def show_home():
     return render_template('index.html')
 
-@app.route('/about_me')
+@app.route('/about_me.html')
 def about_me():
     return render_template('about_me.html')
 
@@ -25,6 +28,13 @@ if __name__ == '__main__':
         freezer = Freezer(app)
         freezer.freeze()
         sys.exit(0)
+    elif opt.test_static:
+        os.chdir('build')
+        PORT = 8000
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = SocketServer.TCPServer(("", PORT), Handler)
+        print("serving at port " + str(PORT))
+        httpd.serve_forever()
     else:
         # Hosting the page
         app.run(
--- playground/utils/options.py
+++ playground/utils/options.py
@@ -3,6 +3,7 @@ import argparse
 def get_options():
     parser = argparse.ArgumentParser("BR's Web Playground")
     parser.add_argument('--test', action='store_true', default=False)
+    parser.add_argument('--test-static', action='store_true', default=False)
     parser.add_argument('--export', action='store_true', default=False)
     parser.add_argument('--host', type=str)
 
